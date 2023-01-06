8dc849e54d2a1cda9fd3b6c18a68b6acfd26ecee
HugoRoca <hugo.rock20@hotmail.com>
2023-01-06 09:12:25 -0500

    end with material desing

--- Material_Desing/app/src/main/java/com/example/material_desing/ScrollingActivity.kt
+++ Material_Desing/app/src/main/java/com/example/material_desing/ScrollingActivity.kt
@@ -1,14 +1,18 @@
 package com.example.material_desing
 
+import android.graphics.Color
 import android.os.Bundle
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.webkit.URLUtil
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.material_desing.databinding.ActivityScrollingBinding
 import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.snackbar.Snackbar
 
 class ScrollingActivity : AppCompatActivity() {
 
@@ -32,6 +36,61 @@ class ScrollingActivity : AppCompatActivity() {
                 .setAnchorView(binding.fabButton)
                 .show()
         }
+
+        binding.contentInclude.btnSkip.setOnClickListener {
+            binding.contentInclude.cvAds.visibility = View.GONE
+        }
+
+        binding.contentInclude.btnBuy.setOnClickListener {
+            Snackbar.make(it, R.string.card_buying, Snackbar.LENGTH_LONG)
+                .setAnchorView(binding.fabButton)
+                .setAction(R.string.card_to_go) {
+                    Toast.makeText(this, R.string.card_history, Toast.LENGTH_SHORT).show()
+                }
+                .show()
+        }
+
+        loadImage("https://imagenes.elpais.com/resizer/s6qH0KXtRg5EEj0IsgRd4sM3GIA=/1200x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/GCWTGAAASNNOJJJG2SZY2HJ2XQ.jpg")
+
+        binding.contentInclude.cbEnablePassword.setOnClickListener {
+            val enabled: Boolean = !binding.contentInclude.tilPassword.isEnabled
+            binding.contentInclude.tilPassword.isEnabled = enabled
+        }
+
+        binding.contentInclude.etUrl.onFocusChangeListener = View.OnFocusChangeListener{ _, focused ->
+            var errorStr: String? = null
+            val url = binding.contentInclude.etUrl.text.toString()
+
+            if (!focused) {
+                if (url.isEmpty()) {
+                    errorStr = getString(R.string.card_required)
+                } else if (URLUtil.isValidUrl(url)) {
+                    loadImage(url)
+                } else {
+                    errorStr = getString(R.string.card_invalid_url)
+                }
+            }
+
+            binding.contentInclude.tilUrl.error = errorStr
+        }
+
+        binding.contentInclude.toggleBottom.addOnButtonCheckedListener { _, checkedId, _ ->
+            binding.contentInclude.root.setBackgroundColor(
+                when (checkedId) {
+                    R.id.btnRed -> Color.RED
+                    R.id.btnBlue -> Color.BLUE
+                    else -> Color.GREEN
+                }
+            )
+        }
+    }
+
+    private fun loadImage(url: String) {
+        Glide.with(this)
+            .load(url)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .centerCrop()
+            .into(binding.contentInclude.imgCover)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
